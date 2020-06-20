cffbaad11c825facfaa58bbc350732fa10abc50f
Xinli Xiao <xiaoxinlixxl@gmail.com>
2020-06-20 17:23:27 -0500

    v0.0.1

--- main.py
+++ main.py
@@ -19,3 +19,23 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+# if __name__ == "__main__":
+#     app.run()
+
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'secret!'
+# socketio = SocketIO(app)
+
+# if __name__ == '__main__':
+#     socketio.run(app)
