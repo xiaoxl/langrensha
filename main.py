e83cca04590b6ea900b18ea50b6ca5056b6e234c
Xinli Xiao <xiaoxinlixxl@gmail.com>
2020-06-20 17:35:37 -0500

    v0.0.0.1

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ socketio = SocketIO(app)
 def sessions():
     return render_template('session.html')
 
+
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-@socketio.on('my event', methods=['GET', 'POST'])
+
+@socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
