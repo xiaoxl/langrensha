9e14feac02ecd2b92133319d23fd49d0f7441279
Xinli Xiao <xiaoxinlixxl@gmail.com>
2020-06-20 20:02:59 -0500

    v0.0.2

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, )
 
 # from flask import Flask
 # app = Flask(__name__)
