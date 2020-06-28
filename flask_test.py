64f84a07e0167cf39a255234dc970fb858238586
Xinli Xiao <xiaoxinlixxl@gmail.com>
2020-06-28 12:35:30 -0500

    v0.0.2 langrensha2 assign roles vote event

--- flask_test.py
+++ flask_test.py
@@ -11,17 +11,18 @@ from flask_socketio import SocketIO, join_room
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
-    #return 'hello world'
+    # return 'hello world'
 
 
 @app.route('/chat')
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
-    
+
     if username and room:
         return render_template('chat.html', username=username, room=room)
     else:
@@ -31,7 +32,9 @@ def chat():
 @socketio.on('send_message')
 def handle_send_message_event(data):
     app.logger.info("{} has sent message to the room {}: {}".format(data['username'], data['room'], data['message']))
-    socketio.emit('receive_message', data, room = data['room'])
+    socketio.emit('receive_message', data, room=data['room'])
+    
+
 
 @socketio.on('join_room')
 def handle_join_room_event(data):
@@ -41,4 +44,4 @@ def handle_join_room_event(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
---
+++ langrensha2.py
@@ -0,0 +1,258 @@
+import json
+import random
+
+
+#%%
+def maxoflist(alist):
+    m = max(alist)
+    return [i for i, j in enumerate(alist) if j == m]
+
+
+class LRSMessage:
+    '''
+    target, sender, receiver are all playernums. 0 means system. The target can represent some other meaning like nvwu.
+    '''
+    def __init__(self, sender=0, receiver=0, target=0, texttype='default'):
+        self.sender = sender
+        self.receiver = receiver
+        self.target = target
+        self.texttype = texttype
+
+    def todict(self):
+        return {'sender': self.sender,
+                'receiver': self.receiver,
+                'target': self.target,
+                'texttype': self.texttype}
+
+
+class User:
+    '''
+    playernum is the most important identifier.
+    '''
+    def __init__(self, username, playernum=0):
+        self.name = username
+        self.role = 'unknown'
+        self.playernum = playernum
+
+    def setrole(self, role):
+        self.role = role
+
+    def setnumber(self, num):
+        self.playernum = num
+
+    def sendmessage(self, target, messagetype, cevent, receiver=0):
+        cevent.update(LRSMessage(sender=self.playernum,
+                                 receiver=receiver, target=target,
+                                 texttype=messagetype))
+
+    def notify(self, text):
+        pass
+
+
+class Event:
+    def __init__(self, relatedusers, targets=[], texttype=''):
+        self.__relatedusers = relatedusers[:]
+        self.__pool = {'A': []}
+        self.__targets = targets
+        self.status = 'Active'
+        self.type = texttype
+        self.result = ['A']
+
+    def update(self, message):
+        sender = message.sender
+        if sender in self.__relatedusers:
+            self.__relatedusers.remove(sender)
+            self.type = message.texttype
+            if message.target in self.__targets:
+                if message.target in self.__pool.keys():
+                    self.__pool[message.target].append(message.sender)
+                else:
+                    self.__pool.update({message.target: [message.sender]})
+            else:
+                self.__pool['A'].append(message.sender)
+        if len(self.__relatedusers) == 0:
+            self.end()
+
+    def end(self):
+        self.status = 'End'
+        pool = [len(p) for p in list(self.__pool.values())]
+        max = maxoflist(pool)
+        self.result = []
+        for index in max:
+            self.result.append(list(self.__pool.keys())[index])
+        if 'A' in self.result:
+            self.result.remove('A')
+
+    def getlog(self):
+        return self.__pool
+
+
+class EventEvenRandom(Event):
+    def __init__(self, relatedusers, targets=[], texttype=''):
+        super().__init__(relatedusers, targets, texttype)
+
+    def end(self):
+        super().end()
+        if len(self.result) > 0:
+            self.result = random.choices(self.result)
+
+
+class EventNvwu(Event):
+    def __init__(self, relatedusers, targets=[], texttype=''):
+        super().__init__(relatedusers, targets, texttype)
+
+    def end(self):
+        super().end()
+        if len(self.result) > 0:
+            self.result = random.choices(self.result)
+
+
+#%%
+class Roles:
+    def __init__(self, name, faction, timing):
+        self.name = name
+        self.faction = faction
+        self.timing = timing
+        self.assigneduser = None
+
+    def night(self):
+        pass
+
+    def passive(self):
+        pass
+
+    def day(self, **kwargs):
+        pass
+
+
+class Roles_Nvwu(Roles):
+    def __init__(self, user):
+        super().__init__('nvwu', 'shen', 30)
+        self.assigneduser = user
+
+    def night(self):
+        print('hohohohoho')
+
+
+class Roles_Yuyanjia(Roles):
+    def __init__(self, user):
+        super().__init__('yuyanjia', 'shen', 40)
+        self.assigneduser = user
+
+    def night(self):
+        print('haha')
+
+
+class Roles_Lieren(Roles):
+    def __init__(self, user):
+        super().__init__('lieren', 'shen', -10)
+        self.assigneduser = user
+
+    def passive(self):
+        print('haha')
+
+
+class Roles_Baichi(Roles):
+    def __init__(self, user):
+        super().__init__('baichi', 'shen', -10)
+        self.assigneduser = user
+
+    def passive(self):
+        print('haha')
+
+
+class Roles_Lang(Roles):
+    def __init__(self, user):
+        super().__init__('lang', 'lang', 20)
+        self.assigneduser = user
+
+    def night(self):
+        print('haha')
+
+
+class Roles_Cunmin(Roles):
+    def __init__(self, user):
+        super().__init__('cunmin', 'min', 0)
+        self.assigneduser = user
+
+
+class Modes:
+    MODE_YNL9 = [Roles_Yuyanjia, Roles_Nvwu, Roles_Lieren, 
+                 Roles_Cunmin, Roles_Cunmin, Roles_Cunmin, 
+                 Roles_Lang, Roles_Lang, Roles_Lang]
+
+    MODE_YNLB = [Roles_Yuyanjia, Roles_Nvwu, Roles_Lieren, Roles_Baichi,
+                 Roles_Cunmin, Roles_Cunmin, Roles_Cunmin, Roles_Cunmin,
+                 Roles_Lang, Roles_Lang, Roles_Lang, Roles_Lang]
+
+    Mode_Default = MODE_YNL9
+
+
+#%%
+class GameStatus:
+    def __init__(self, user=[], modes=Modes.Mode_Default):
+        self.Modes = modes
+        self.NUMBEROFPLAYERS = len(user)
+        self.allusers = user
+        self.roles = []
+        tempuser = self.allusers[:]
+        random.shuffle(tempuser)
+        for i, p in enumerate(tempuser):
+            self.roles.append(modes[i](p))
+        self.roleindex = {}
+        for i in self.roles:
+            i.assigneduser.setrole(i.name)
+            if i.name in self.roleindex.keys():
+                temlist = self.roleindex[i.name]
+                temlist.append(i.assigneduser)
+                self.roleindex.update({i.name: temlist})
+            else:
+                self.roleindex.update({i.name: [i.assigneduser]})
+
+    def changeroles(self):
+        tempuser = self.allusers[:]
+        random.shuffle(tempuser)
+        for i, p in enumerate(tempuser):
+            self.roles.append(self.Modes[i](p))
+
+    def printroles(self):
+        for r in list(self.roleindex.keys()):
+            for u in self.roleindex[r]:
+                print(r + ': ' + u.name)
+
+
+new = Event(relatedusers=[1, 2, 3], targets=[1, 2])
+
+user1 = User(username='a1', playernum=1)
+user2 = User(username='b2', playernum=2)
+user3 = User(username='c3', playernum=3)
+user4 = User(username='d4', playernum=4)
+user5 = User(username='e5', playernum=5)
+user6 = User(username='f6', playernum=6)
+user7 = User(username='g7', playernum=7)
+user8 = User(username='h8', playernum=8)
+user9 = User(username='i9', playernum=9)
+
+user8.role
+
+
+#%%
+users = [user1, user2, user3, user4, user5, user6, user7, user8, user9]
+
+newgame = GameStatus(users)
+newgame.printroles()
+
+newgame.roles[0].night()
+user8.role
+
+#%%
+user1.sendmessage(1, 's', new)
+print(new.getlog())
+#%%
+user1.sendmessage(1, 's', new)
+print(new.getlog())
+user3.sendmessage(2, 's', new)
+print(new.getlog())
+user2.sendmessage(3, 's', new)
+print(new.getlog())
+print(new.result)
